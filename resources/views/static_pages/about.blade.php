3135b5ad631e649dcc87e8c4aea47c2a6b81eafb
guava1986 <dalvpeng@gmail.com>
2018-11-07 15:31:05 +0000

    Poetry

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.default')
 @section('title','关于')
 @section('content')
-<h1>关于页</h1>
+<div align="center">
+<p>
+    <h3>立冬后</h3>
+    <br>
+    <br>
+
+©麥冬<br>
+<br>
+
+這應該是最後一場雨<br>
+一個秋天我們都沒有用過傘<br>
+在雨中我們習慣穿行<br>
+習慣性傾聽天空的訴説<br>
+<br>
+<br>
+去年的最後一場雨<br>
+在大街上唳寒<br>
+滿眼的水即將決眶<br>
+伶仃的身影在菊過的風裏<br>
+褪去生活的泥沙<br>
+<br>
+<br>
+需要哭時<br>
+可以把頭仰起<br>
+我們經歷過淬火<br>
+經歷過沉默<br>
+讓自己瘦骨嶙峋<br>
+<br>
+<br>
+大雪不一定寒冷<br>
+多年前我已立冬<br>
+</p>
+</div>
 @stop
